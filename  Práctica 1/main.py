abd19b7ce1229fb385ac539bf0d829f98b0943bb
Eduardo Catalán <marvineduardocv12@gmail.com>
2020-12-06 22:17:02 -0600

    Ajustes y validaciones mínimas

---  Práctica 1/main.py
+++  Práctica 1/main.py
@@ -30,34 +30,31 @@ def InvalidOp(op, limI, limS):
 #Función para leer todos los datos del CSV y distribuirlos en una lista de objetos de tipo DATA con los atributos creados anteriormente
 def ReadingData():
     global lines , line, recordList, lineSplit,idList
+    contador=0
     nameFile =  input('Escriba la ruta o el nombre(como ruta relativa) del archivo csv debe de'
                       ' ingresar un nombre de ls siguiente manera(ejemplo.csv): \n')
+    trucado = nameFile.split(".")
+    if trucado[1] == 'CSV' or trucado[1] == 'csv':
+        fileCSV = open(nameFile,'r',encoding='UTF-8')#Abro el archivo en modo lectura
 
-    fileCSV = open(nameFile,'r',encoding='UTF-8')#Abro el archivo en modo lectura
-
-    lines = fileCSV.readlines()#Guardo todas las lineas del CSV en un solo vector para luego manipularlo
-    fileCSV.close()  # Cierro el archivo
-    for line in lines:
-        if line != lines[0]:
-            lineSplit = line.split(",")
-            if lineSplit[0] not in idList:
-                idList.append(lineSplit[0])#Creo una lista de Id para filtrar luego los que se vayan añadiendo
-                recordList.append(Data(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4], lineSplit[5]))
-                # Aqui ya tengo una lista con todos los registros y sus debidos atributos
-            elif (lineSplit[0] in idList):
-                print("\nSe ha  encontrado un Id repetido!!!\n")
-            if lineSplit[4] not in placeList:
-                placeList.append(lineSplit[4])
-
-    for printe in recordList:
-        print(":::Resumen de datos procesados y filtrados:::")
-        print(printe.id)
-        print(printe.name)
-        print(printe.lastName)
-        print(printe.age)
-        print(printe.place)
-        print(printe.salary+'\n')
-    print("\nArchivo CSV leído y procesado con éxito!!!\n")
+        lines = fileCSV.readlines()#Guardo todas las lineas del CSV en un solo vector para luego manipularlo
+        fileCSV.close()  # Cierro el archivo
+        for line in lines:
+            if line != lines[0]:
+                lineSplit = line.split(",")
+                if lineSplit[0] not in idList:
+                    idList.append(lineSplit[0])#Creo una lista de Id para filtrar luego los que se vayan añadiendo
+                    recordList.append(Data(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4], lineSplit[5]))
+                    # Aqui ya tengo una lista con todos los registros y sus debidos atributos
+                elif (lineSplit[0] in idList):
+                    contador+=1
+                if lineSplit[4] not in placeList:
+                    placeList.append(lineSplit[4])
+        if contador > 0:
+            print("\nSe ha  encontrado Id(s) repetido(s)!!! TOTAL= " + str(contador))
+        print("\nArchivo CSV leído procesado y filtrado con éxito!!!\n")
+    elif trucado[1] != "CSV" or trucado[1] != "csv" or trucado[1] != " ":
+        print("\nEl archivo que estas tratando de ingresar no es CSV\n")
 
 def conectDictionary(list): #Creo una función a la cual le paso una lista de diccionarios para unirlos en un solo diccionario
 
@@ -89,9 +86,11 @@ def Calculations():
                     list.append(dictionary)
             temporalList.append(temporalPlace)
             datass = conectDictionary(list)
+        print("\nCalculos realizados correctamente, ya puede generar archivo Json!!!\n")
 def generateJson():
     with open('Datos Procesados.json','w',encoding="utf-8") as f:
         json.dump(datass,f, ensure_ascii= False,indent=4)
+    print("\nArchivo Json creado con éxito!!!\n")
 def MainMenu():
     op=0
     while op < 1 or op > 4:
